9aa8f7f5aa86da96d6cf80b3a5920cfa08189d14
Farangis <yfarangis@gmail.com>
2022-04-15 17:50:10 +0500

    New project step 1

---
+++ Checklists/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by MMM on 15/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
